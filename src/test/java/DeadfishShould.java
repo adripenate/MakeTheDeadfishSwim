39dcd41271c63ba38233f2e01e5773db2759fe78
adriana <adrianapenate98@gmail.com>
2020-01-22 10:47:50 +0000

    Red test - only_output_a_zero

--- src/test/java/DeadfishShould.java
+++ src/test/java/DeadfishShould.java
@@ -1,7 +1,16 @@
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DeadfishShould {
     @Test
-    public void name() {
+    public void only_output_a_zero() {
+        assertThat(Deadfish.parse("o")).isEqualTo(new int[] {0});
+    }
+
+    private static class Deadfish {
+        public static int[] parse(String o) {
+            return new int[] {};
+        }
     }
 }
